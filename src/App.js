859e4d9babac1fcedd2fbd7fba8aae924ffe8c3e
HoaiNhu <22521051@gm.uit.edu.vn>
2024-11-02 21:25:13 +0700

    logo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routes } from "./routes";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
