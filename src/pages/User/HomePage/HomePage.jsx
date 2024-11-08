33f0b5a0ad664c382518859e5ee56c57829464d1
DiemNganNguyenNgoc <22520933@gm.uit.edu.vn>
2024-11-08 11:31:29 +0700

    add CardProduct

--- src/pages/User/HomePage/HomePage.jsx
+++ src/pages/User/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import SliderComponent from '../../../components/SliderComponent/SliderComponent
 import slider1 from '../../../assets/img/slider1.webp'
 import slider2 from '../../../assets/img/slider2.webp'
 import slider3 from '../../../assets/img/slider3.webp'
+import CardProduct from '../../../components/CardProduct/CardProduct'
 
 
 const HomePage = () => {
@@ -10,6 +11,9 @@ const HomePage = () => {
     <div>
       <div>
         <SliderComponent arrImg={[slider1, slider3, slider2 ]}/>
+      </div>
+      <div>
+        <CardProduct>Card 1</CardProduct>
       </div>
     </div>
   )
