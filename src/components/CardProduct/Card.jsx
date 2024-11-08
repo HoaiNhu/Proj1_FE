cebc8f4350bc8d29ec513a69ecbdb1c95479ab0e
DiemNganNguyenNgoc <22520933@gm.uit.edu.vn>
2024-11-08 16:52:52 +0700

    Sanphamnoibat

---
+++ src/components/CardProduct/Card.jsx
@@ -0,0 +1,16 @@
+import Card from 'react-bootstrap/Card';
+import './CardProduct.css';
+
+const CardProduct = ({ img, title, price }) => {
+  return (
+    <Card style={{ width: '23rem' }}>
+      <Card.Img src={img} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Subtitle>{price}</Card.Subtitle>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default CardProduct;
