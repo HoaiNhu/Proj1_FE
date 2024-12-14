e1cc99eb798118a5ccd765672c9ed1c73ff0c655
HoaiNhu <22521051@gm.uit.edu.vn>
2024-12-14 11:45:37 +0700

    xu-li-token-het-han

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ import WebFont from "webfontloader";
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css";
 import "bootstrap-datepicker";
-
 import "./assets/css/reset.css";
 import "./assets/css/style.css";
 import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
 import FooterComponent from "./components/FooterComponent/FooterComponent";
 import { routes } from "./routes";
 import { AuthProvider } from "./context/AuthContext";
+import { isJsonString } from "./utils";
+import { jwtDecode } from "jwt-decode";
+import { updateUser } from "./redux/slides/userSlide";
+import * as UserService from "./services/UserService";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
   useEffect(() => {
     WebFont.load({
       google: {
@@ -23,6 +28,48 @@ function App() {
       },
     });
   }, []);
+
+  useEffect(() => {
+    const { storageData, decoded } = handleDecoded();
+
+    if (decoded?.id) {
+      handleGetDetailsUser(decoded?.id, storageData);
+    }
+  }, []);
+
+  const handleDecoded = () => {
+    let storageData = localStorage.getItem("access_token");
+    let decoded = {};
+    if (storageData && isJsonString(storageData)) {
+      storageData = JSON.parse(storageData);
+      decoded = jwtDecode(storageData);
+    }
+    return { decoded, storageData };
+  };
+
+  //token hết hạn
+  UserService.axiosJWT.interceptors.request.use(
+    async (config) => {
+      // Do something before request is sent
+      const currentTime = new Date();
+      const { decoded } = handleDecoded();
+      if (decoded?.exp < currentTime.getTime() / 1000) {
+        const data = await UserService.refreshToken();
+        config.headers["token"] = `Bearer ${data?.access_token}`;
+      }
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+
+  const handleGetDetailsUser = async (id, token) => {
+    const res = await UserService.getDetailsUser(id, token);
+    // console.log("res", res);
+    dispatch(updateUser({ ...res?.data, access_token: token }));
+  };
 
   // useEffect(() => {
   //   fetchApi();
