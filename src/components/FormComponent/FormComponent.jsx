9a64ccad73f9d2f96fcdd979799a935797a72691
DiemNganNguyenNgoc <22520933@gm.uit.edu.vn>
2024-12-17 08:14:55 +0700

    formcomponent

--- src/components/FormComponent/FormComponent.jsx
+++ src/components/FormComponent/FormComponent.jsx
@@ -4,23 +4,27 @@ import styles from "./FormComponent.module.css";
 const FormComponent = ({
   className = "",
   style = {},
-  value,
-  onChange,
-  error,
+  value = "",
+  onChange = () => {},
+  error = false,
+  name = "",
+  placeholder = "",
   ...props
 }) => {
   return (
-    <div className={`${styles.form__control}`}>
+    <div className={`${styles.form__control} ${className}`}>
       <input
-        type={props.type}
-        className={` ${styles.form__text}`}
-        id={props.id}
-        placeholder={props.placeholder}
+        type={props.type || "text"}
+        className={`${styles.form__text}`}
+        id={props.id || name} // ưu tiên name làm id nếu không có id
+        name={name} // thêm thuộc tính name
+        placeholder={placeholder}
         style={style}
         value={value}
-        onChange={(e) => onChange && onChange(e.target.value)}
+        onChange={(e) => onChange(e)} // truyền toàn bộ event
         {...props}
       />
+      {error && <span className={styles.error__text}>{error}</span>}
     </div>
   );
 };
