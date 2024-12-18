dde378dfda8266cac67bfd66de3e69c4a20b6070
HoaiNhu <22521051@gm.uit.edu.vn>
2024-12-18 16:53:05 +0700

    CRUD-status

--- src/components/ButtonComponent/ButtonComponent.jsx
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -14,15 +14,15 @@ const ButtonComponent = ({
   const fileInputRef = useRef(null); // Tạo tham chiếu đến input file
 
   const handleClick = (event) => {
+    if (to) {
+      event.preventDefault(); // Ngăn form hoặc hành động khác nếu có
+      navigate(to); // Điều hướng đến trang chỉ định
+    }
     if (props.onClick) {
       props.onClick(event); // Gọi thêm hàm onClick nếu được truyền
     }
     if (fileInputRef.current) {
       fileInputRef.current.click(); // Kích hoạt click trên input file
-    }
-    if (to) {
-      event.preventDefault(); // Ngăn form hoặc hành động khác nếu có
-      navigate(to); // Điều hướng đến trang chỉ định
     }
   };
 
--- src/services/StatusService.js
+++ src/services/StatusService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosJWT = axios.create();
 
-export const createStatus = async (data) => {
+export const createStatus = async (data, access_token) => {
   try {
     const res = await axios.post(
       `${process.env.REACT_APP_API_URL_BACKEND}/status/create-status`,
@@ -10,6 +10,7 @@ export const createStatus = async (data) => {
       {
         headers: {
           "Content-Type": "application/json",
+          token: `Bearer ${access_token}`,
         },
       }
     );
@@ -26,11 +27,10 @@ export const createStatus = async (data) => {
   }
 };
 
-
 export const getDetailsStatus = async (id, access_token) => {
   try {
     const res = await axiosJWT.get(
-      `${process.env.REACT_APP_API_URL_BACKEND}/user/get-detail-status/${id}`,
+      `${process.env.REACT_APP_API_URL_BACKEND}/status/get-detail-status/${id}`,
       {
         headers: {
           "Content-Type": "application/json",
@@ -38,6 +38,102 @@ export const getDetailsStatus = async (id, access_token) => {
         },
       }
     );
+    return res.data;
+  } catch (error) {
+    if (error.response) {
+      throw {
+        // status: error.response.data?.status || "ERR",
+        message: error.response.data?.message || "Đã xảy ra lỗi.",
+      };
+    } else {
+      throw { status: 500, message: "Không thể kết nối đến máy chủ." };
+    }
+  }
+};
+
+export const getAllStatus = async (access_token) => {
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_API_URL_BACKEND}/status/get-all-status`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          token: `Bearer ${access_token}`,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    if (error.response) {
+      throw {
+        // status: error.response.data?.status || "ERR",
+        message: error.response.data?.message || "Đã xảy ra lỗi.",
+      };
+    } else {
+      throw { status: 500, message: "Không thể kết nối đến máy chủ." };
+    }
+  }
+};
+
+export const updateStatus = async (id, data, access_token) => {
+  try {
+    const res = await axios.put(
+      `${process.env.REACT_APP_API_URL_BACKEND}/status/update-status/${id}`,
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          token: `Bearer ${access_token}`,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    const errorMessage =
+      error.response?.data?.message || "Không thể cập nhật trạng thái.";
+    throw new Error(errorMessage); // Ném lỗi dạng chuỗi thay vì object
+  }
+};
+
+export const deleteStatus = async (id, access_token) => {
+  try {
+    const res = await axios.delete(
+      `${process.env.REACT_APP_API_URL_BACKEND}/status/delete-status/${id}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          token: `Bearer ${access_token}`,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    if (error.response) {
+      throw {
+        // status: error.response.data?.status || "ERR",
+        message: error.response.data?.message || "Đã xảy ra lỗi.",
+      };
+    } else {
+      throw { status: 500, message: "Không thể kết nối đến máy chủ." };
+    }
+  }
+};
+
+export const refreshToken = async () => {
+  try {
+    const res = await axios.post(
+      `${process.env.REACT_APP_API_URL_BACKEND}/status/refresh-token`,
+      {},
+      {
+        // headers: {
+        //   "Content-Type": "application/json",
+        //   token: `Bearer ${access_token}`,
+        // },
+        withCredentials: true, //tự động lấy cookie
+      }
+    );
+    // console.log("res.data", res.data);
+
     return res.data; // Trả dữ liệu nếu thành công
   } catch (error) {
     // Nếu API trả về lỗi, ném lỗi với thông tin chi tiết
@@ -49,30 +145,6 @@ export const getDetailsStatus = async (id, access_token) => {
       };
     } else {
       // Lỗi không có response (ví dụ lỗi mạng)
-      throw { status: 500, message: "Không thể kết nối đến máy chủ." };
-    }
-  }
-};
-
-export const updateStatus = async (data) => {
-  try {
-    const res = await axios.post(
-      `${process.env.REACT_APP_API_URL_BACKEND}/status/update-status`,
-      data,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    return res.data;
-  } catch (error) {
-    if (error.response) {
-      throw {
-        // status: error.response.data?.status || "ERR",
-        message: error.response.data?.message || "Đã xảy ra lỗi.",
-      };
-    } else {
       throw { status: 500, message: "Không thể kết nối đến máy chủ." };
     }
   }
